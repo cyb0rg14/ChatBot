9fc844f6bd004d4e652d343a9a50b48ff95de62b
Tech Titan <techtitan1406@gmail.com>
2023-08-22 05:15:07 +0530

    changed the way bot replies

--- webapp.py
+++ webapp.py
@@ -91,7 +91,7 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder=st.empty()
             full_response=''
             for item in response:
-                full_response+=item
+                full_response = full_response + "\n" + item
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
 
